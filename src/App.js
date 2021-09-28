06eb20abbfaf3e78db83448114a318aa723f78cc
jmferg11 <jfergie93@gmail.com>
2021-09-28 11:53:25 -0400

    add Petunia module, troubleshooting image connection

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Structure from './structure';
 import PhotoDisplay from './photo-portion';
 import Bio from './bio';
+import Petunia from './petunia';
 
 function App() {
   return (
     <Router>
       <Structure>
         <PhotoDisplay />
+        <Petunia />
           
         <Bio />
       </Structure>
---
+++ src/petunia.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Petunia = (props) => {
+    return (
+        <React.Fragment>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-8'>
+                        <h4>photo1</h4>
+                    </div>
+                    <div className='col-md-2'>
+                        <h5>description</h5>
+                    </div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-8'>
+                        <h4>photo1</h4>
+                    </div>
+                    <div className='col-md-2'>
+                        <h5>description</h5>
+                    </div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-8'>
+                        <h4>photo1</h4>
+                    </div>
+                    <div className='col-md-2'>
+                        <h5>description</h5>
+                    </div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-8'>
+                        <h4>photo1</h4>
+                    </div>
+                    <div className='col-md-2'>
+                        <h5>description</h5>
+                    </div>
+                    <div className='col-md-1'></div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Petunia
