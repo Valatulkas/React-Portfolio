e8609ec82bb05b43daef40cb881a55605460ec71
jmferg11 <jfergie93@gmail.com>
2021-09-27 22:40:08 -0400

    add footer links

--- src/structure.js
+++ src/structure.js
@@ -5,26 +5,43 @@ const Structure = (props) => {
         <React.Fragment>
             <nav className='navbar navbar-expand-lg py-0'>
                 <div className='container'>
-                    <div className='col-12 col-md-6'>
-                        <h1>JFerg</h1>
-                    </div>
-                    <div className='col-12 col-md-2'>
-                        <h3>Projects</h3>
-                    </div>
-                    <div className='col-12 col-md-2'>
-                        <h3>Photos</h3>
-                    </div>
-                    <div className='col-12 col-md-2'>
-                        <h3>About</h3>
-                    </div>
+                        <div className='col-12 col-md-6'>
+                            <h1>JFerg</h1>
+                        </div>
+                        <div className='col-12 col-md-2'>
+                            <h3>Projects</h3>
+                        </div>
+                        <div className='col-12 col-md-2'>
+                            <h3>Photos</h3>
+                        </div>
+                        <div className='col-12 col-md-2'>
+                            <h3>About</h3>
+                        </div>
                 </div>
             </nav>
             <div className='container' id='main-content'>
                 {props.children}
             </div>
-            <footer>
-                <div>
-
+            <footer className='footer'>
+                
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-12 col-md-4'>
+                            <a href='https://github.com/Valatulkas'>
+                                <img src='./images/github-seeklogo.com.svg' alt='github' className='mb-4'></img>
+                            </a>
+                        </div>
+                        <div className='col-12 col-md-4'>
+                            <a href="mailto:jfergie93@gmail.com">
+                                <h4 className='mb-4'>email</h4>
+                            </a>
+                        </div>
+                        <div className='col-12 col-md-4'>
+                            <a href='https://www.chess.com'>
+                                <h4 className='mb-4'>chess</h4>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </footer>
         </React.Fragment>
