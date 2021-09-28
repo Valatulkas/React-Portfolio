7dc602aef7fca2e5896460d75da177a20c390285
jmferg11 <jfergie93@gmail.com>
2021-09-27 21:55:21 -0400

    add navbar outline

--- src/structure.js
+++ src/structure.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const Structure = (props) => {
     return (
         <React.Fragment>
-            <nav>
-
+            <nav className='navbar navbar-expand-lg py-0'>
+                <div className='container'>
+                    <div className='col-12 col-md-6'>
+                        <h1>JFerg</h1>
+                    </div>
+                    <div className='col-12 col-md-2'>
+                        <h3>Projects</h3>
+                    </div>
+                    <div className='col-12 col-md-2'>
+                        <h3>Photos</h3>
+                    </div>
+                    <div className='col-12 col-md-2'>
+                        <h3>About</h3>
+                    </div>
+                </div>
             </nav>
             <div className='container' id='main-content'>
                 {props.children}
             </div>
             <footer>
+                <div>
 
+                </div>
             </footer>
         </React.Fragment>
     )
-}
+}   
 
 export default Structure
