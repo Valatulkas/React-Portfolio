5120cb4debc3c38e530f1380318bbcb18f0ea0ad
jmferg11 <jfergie93@gmail.com>
2021-10-14 18:01:12 -0400

    add borders and light styling to webdev page

--- src/projects.js
+++ src/projects.js
@@ -13,54 +13,66 @@ const Project = (props) => {
                         <h1>WEBDEV PROJECTS</h1>
                 </div>
                 <div className='row'>
-                    <div className='col-12'>
-                        <h4>HTML - CSS3 Clone Sites</h4>
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 my-4'>
+                        <h2 className='project-title'><b>HTML - CSS3 Clone Sites</b></h2>
                     </div>
+                    <div className='col-0 col-md-2'></div>
 
-                    <div className='col-md-12' id='webdev'>
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 pt-2' id='webdev'>
                         <a href='https://eager-lamarr-f0647c.netlify.app/' target="_blank">
                             <img src={ apple } width='788px' height='auto' />
                             <h5>Apple</h5>
                             <br/>
                         </a>
                     </div>
+                    <div className='col-0 col-md-2'></div>
 
-                    <div className='col-md-12' id='webdev'>
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 pt-2 mt-4' id='webdev'>
                         <a href='https://romantic-raman-89cffe.netlify.app/' target="_blank">
                             <img src={ newsweek } width='788px' height='auto' />
                             <h5>Newsweek</h5>
                             <br/>
                         </a>
                     </div>
+                    <div className='col-0 col-md-2'></div>
 
-                    <div className='col-md-12' id='webdev'>
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 pt-2 my-4' id='webdev'>
                         <a href='https://optimistic-jepsen-9be5a6.netlify.app/' target="_blank">
                             <img src={ airbnb } width='788' height='auto' /> 
                             <h5>AirBNB Newsroom</h5>
                             <br/>
                         </a>
                     </div>
+                    <div className='col-0 col-md-2'></div>
 
-                    <hr/>
+                    <hr className='my-5'/>
 
                     <div className='col-12'>
-                        <h4>JS - React.js</h4>
+                        <h2 className='project-title pb-3'><b>JS - React.js</b></h2>
                     </div>
-                    <div className='col-md-12' id='webdev'>
+
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 pt-4' id='webdev'>
                         <a href='https://jovial-jepsen-0d057e.netlify.app/' target="_blank">
                             <img src={ currency } width='688' height='auto' />
                             <h5>React Currency Converter</h5>
                             <br/>
                         </a>
                     </div>
-                    <br/>
+                    <div className='col-0 col-md-2'></div>
 
-                    <div className='col-md-12' id='webdev'>
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 pt-4 my-3' id='webdev'>
                         <a href='https://peaceful-hoover-ec2589.netlify.app/' target="_blank">
                             <img src={ mathgame } width='388' height='auto' />
                             <h5>Simple Math Game</h5>
                         </a>
                     </div>
+                    <div className='col-0 col-md-2'></div>
                 </div>
                 <div className='row' id='banner1'>
                         <h1>WEBDEV PROJECTS</h1>
