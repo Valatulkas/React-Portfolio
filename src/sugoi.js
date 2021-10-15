08d71d2e3a611cb508701ea7ca852d000f62ec11
jmferg11 <jfergie93@gmail.com>
2021-10-15 19:25:16 -0400

    restyle sugoi page

--- src/sugoi.js
+++ src/sugoi.js
@@ -11,58 +11,56 @@ const Sugoi = (props) => {
             <div className='container main sugoi'>
                 <div className='row py-4'>
                     <div className='col-md-12'>
-                        <h3 className='sugoi-title mb-5'>**sugoi (japanese) translates to awesome** <br/> collection of personal projects</h3>
+                        <h3 className='sugoi-title mb-5'>~ sugoi (japanese) translates to awesome ~<br/>collection of personal projects</h3>
                     </div>
 
-                    <div className='col-md-7'>
-                        <img src={ feesh } alt='feesh' width='300px' height='auto' />
+                    <div className='col-md-6'>
+                        <div id='webdev-fun'>
+                            <a>
+                                <img src={ feesh } alt='feesh' width='400px' height='auto' />
+                                <Link to='./corco'>
+                                    <h4>watery plains</h4>
+                                </Link>
+                            </a>
+                        </div>
                     </div>
-                    <div className='col-md-5' id='webdev-fun'>
-                        <a>
-                        <Link to='./corco'>
-                            <h4>watery plains</h4>
-                        </Link>
-                        </a>
-                    </div>
-
-                    <hr className='my-3' />
                 
-                    <div className='col-md-7'>
-                        <img src={ snowy } alt='snowy-japan' width='300px' height='auto' />
-                    </div>
-                    <div className='col-md-5' id='webdev-fun'>
-                        <a>
-                        <Link to='./yapon'>
-                            <h4>hokkaido hospitality</h4>
-                        </Link>
-                        </a>
+                    <div className='col-md-6'>
+                        <div id='webdev-fun'>
+                            <a>
+                                <img src={ snowy } alt='snowy-japan' width='400px' height='auto' />
+                                <Link to='./yapon'>
+                                    <h4>hokkaido hospitality</h4>
+                                </Link>
+                            </a>
+                        </div>
                     </div>
 
-                    <hr className='my-3' />
+                    <hr className='my-5'/>
 
-                    <div className='col-md-7'>
-                        <img src={ work } alt='petunia1' width='300px' height='auto' />
+                    <div className='col-md-6'>
+                        <div id='webdev-fun'>
+                            <a>
+                                <img src={ work } alt='petunia1' width='400px' height='auto' />
+                                <Link to='./petunia'>
+                                    <h4>Trials and tribulations of restoring an AWD van</h4>
+                                </Link>
+                            </a>
+                        </div>
                     </div>
-                    <div className='col-md-5' id='webdev-fun'>
-                        <a>
-                            <Link to='./petunia'>
-                                <h4>Trials and tribulations of restoring an AWD van</h4>
-                            </Link>
-                        </a>
+                
+                    
+                    <div className='col-md-6 pt-5'>
+                        <div id='webdev-fun'>
+                            <a>
+                                <img src={ sneaky } alt='barrel' width='400px' height='auto' />
+                                <Link to='./wuter'>
+                                    <h4>sneaky</h4>
+                                </Link>
+                            </a>
+                        </div>
                     </div>
                     
-                    <hr className='my-3' />
-                    
-                    <div className='col-md-7'>
-                        <img src={ sneaky } alt='barrel' width='300px' height='auto' />
-                    </div>
-                    <div className='col-md-5' id='webdev-fun'>
-                        <a>
-                        <Link to='./wuter'>
-                            <h4>sneaky</h4>
-                        </Link>
-                        </a>
-                    </div>
                 </div>
             </div>
 
