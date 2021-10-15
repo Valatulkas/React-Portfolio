d9859414e9850b77e4229af0da650970c1b730ac
jmferg11 <jfergie93@gmail.com>
2021-10-15 18:48:07 -0400

    additional bio styling

--- src/bio.js
+++ src/bio.js
@@ -10,14 +10,15 @@ const Bio = (props) => {
                     <div className='col-12 col-md-10' id='bio' style={{backgroundImage: `url(${ subway })`}}>
                         <div className='bio1-parent'>
                             <div id='bio1'>
-                                <h4>ASPIRING FULL STACK WEB DEVELOPER</h4>
-                                <h4>FINISHING FULL STACK BOOTCAMP THROUGH ALTCADEMY</h4>
-                                <h4>AIMING TO PROGRESS ENVIRONMENTAL AGENDAS</h4>
-                                <h4>SPECIFICALLY SLOW FOODS AND RENEWABLE ENERGIES</h4>
+                                <h4>aspiring full stack developer</h4>
+                                <h4>near completion of Altcademy full stack bootcamp</h4>
+                                <h4>aiming to progress environmental agendas</h4>
+                                <h4>specifically slow food and renewable energies</h4>
                             </div>
                         </div>
                     </div>
                     <div className='col-0 col-md-1'></div>
+                    <p id='synopsis' className='mt-5'>surfin in the NorthEastern USA in the meantime... might find me in Long Island waters pending the below!</p>
                 </div>
             </div>
         </React.Fragment>
