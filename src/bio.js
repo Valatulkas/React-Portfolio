9f06ed5e2e9454761b829d2e32d0140d5f605bdf
jmferg11 <jfergie93@gmail.com>
2021-10-15 22:17:13 -0400

    need to find blanket styling across pages

--- src/bio.js
+++ src/bio.js
@@ -11,14 +11,15 @@ const Bio = (props) => {
                         <div className='bio1-parent'>
                             <div id='bio1'>
                                 <h4>aspiring full stack developer</h4>
-                                <h4>near completion of Altcademy full stack bootcamp</h4>
+                                <br/>
                                 <h4>aiming to progress environmental agendas</h4>
+                                <br/>
                                 <h4>specifically slow food and renewable energies</h4>
                             </div>
                         </div>
                     </div>
                     <div className='col-0 col-md-1'></div>
-                    <p id='synopsis' className='mt-5'>surfin in the NorthEastern USA in the meantime... might find me in Long Island waters pending the below!</p>
+                    <h3 id='synopsis' className='mt-5'>surfin in the NorthEastern USA in the meantime... might find me in waters off the Long Island coast</h3>
                 </div>
             </div>
         </React.Fragment>
--- src/projects.js
+++ src/projects.js
@@ -23,7 +23,7 @@ const Project = (props) => {
                     <div className='col-12 col-md-8 pt-2' id='webdev'>
                         <a href='https://eager-lamarr-f0647c.netlify.app/' target="_blank">
                             <img src={ apple } width='788px' height='auto' />
-                            <h5>Apple</h5>
+                            <h3>--- Apple ---</h3>
                             <br/>
                         </a>
                     </div>
@@ -33,7 +33,7 @@ const Project = (props) => {
                     <div className='col-12 col-md-8 pt-2 mt-4' id='webdev'>
                         <a href='https://romantic-raman-89cffe.netlify.app/' target="_blank">
                             <img src={ newsweek } width='788px' height='auto' />
-                            <h5>Newsweek</h5>
+                            <h3>--- Newsweek ---</h3>
                             <br/>
                         </a>
                     </div>
@@ -43,7 +43,7 @@ const Project = (props) => {
                     <div className='col-12 col-md-8 pt-2 my-4' id='webdev'>
                         <a href='https://optimistic-jepsen-9be5a6.netlify.app/' target="_blank">
                             <img src={ airbnb } width='788' height='auto' /> 
-                            <h5>AirBNB Newsroom</h5>
+                            <h3>--- AirBNB Newsroom ---</h3>
                             <br/>
                         </a>
                     </div>
@@ -59,7 +59,7 @@ const Project = (props) => {
                     <div className='col-12 col-md-8 pt-4' id='webdev'>
                         <a href='https://jovial-jepsen-0d057e.netlify.app/' target="_blank">
                             <img src={ currency } width='688' height='auto' />
-                            <h5>React Currency Converter</h5>
+                            <h3>--- React Currency Converter ---</h3>
                             <br/>
                         </a>
                     </div>
@@ -69,7 +69,7 @@ const Project = (props) => {
                     <div className='col-12 col-md-8 pt-4 my-3' id='webdev'>
                         <a href='https://peaceful-hoover-ec2589.netlify.app/' target="_blank">
                             <img src={ mathgame } width='388' height='auto' />
-                            <h5>Simple Math Game</h5>
+                            <h3>--- Simple Math Game ---</h3>
                         </a>
                     </div>
                     <div className='col-0 col-md-2'></div>
