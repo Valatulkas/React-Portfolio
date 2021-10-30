000d978d2d0c5277745069d0bb4869de33a889f0
jmferg11 <jfergie93@gmail.com>
2021-10-30 19:35:44 -0400

    create rails component

---
+++ src/railsprojects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import todolist from './images/ursl/todolist.png';
+
+const Railsprojects = () => {
+    return (
+        <React.Fragment>
+            <div></div>
+        </React.Fragment>
+    )
+}
