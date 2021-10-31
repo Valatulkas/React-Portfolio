96dc45ce3e2c934a19404638a826a31215072a1a
jmferg11 <jfergie93@gmail.com>
2021-10-30 23:09:35 -0400

    add hr separation in bio

--- src/photo-portion.js
+++ src/photo-portion.js
@@ -5,7 +5,7 @@ const IntroDisplay = (props) => {
     return (
         <React.Fragment>
             <Bio />
-
+            <hr className='my-5' id='separate' />
             <div className='container main my-5 photo-background'>
                 <div id='forecast'>
                     <link href="//www.surf-forecast.com/stylesheets/widget.css" media="screen" rel="stylesheet" type="text/css" /><div class="wf-width-cont surf-fc-widget"><div class="widget-container"><div class="external-cont"><iframe class="surf-fc-i" allowtransparency="true" src="//www.surf-forecast.com/breaks/Road-K/forecasts/widget/a" scrolling="no" frameborder="0" marginwidth="0" marginheight="0"></iframe><div class="footer"><a class="logo" href="//www.surf-forecast.com/"><img src="//www.surf-forecast.com/images/widget.png" alt="Widget" width="1" height="1" /></a><div class="about" id="cmt">More <a href="//www.surf-forecast.com/breaks/Road-K">Detailed Surf Conditions &amp; Webcams for&nbsp;Road K</a> <nobr>at&nbsp;<a href="//www.surf-forecast.com/">surf-forecast.com</a></nobr>.</div></div></div></div></div>
