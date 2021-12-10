d7bff08db20454595ea982f6bc2a3d676521ad82
jmferg11 <jfergie93@gmail.com>
2021-12-10 09:47:38 -0500

    add twitter clone to rails page

--- src/railsprojects.js
+++ src/railsprojects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import todolist from './images/urls/todolist.png';
+import twitter from './images/urls/twitter.png';
 
 const Railsprojects = () => {
     return (
@@ -7,21 +8,31 @@ const Railsprojects = () => {
             <div className='container main'>
                 <div className='row'>
                     <div className='col-0 col-md-2'></div>
-                        <div className='col-12 col-md-8'>
-                            <br/>
-                            <h1 className='project-title my-4'><b>Ruby on Rails</b></h1>
-                        </div>
-                        <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8'>
+                        <br/>
+                        <h1 className='project-title my-4'><b>Ruby on Rails</b></h1>
+                    </div>
+                    <div className='col-0 col-md-2'></div>
 
-                        <div className='col-0 col-md-2'></div>
-                        <div className='col-12 col-md-8 pt-5 mb-3' id='webdev'>
-                            <a href='https://jf-fullstack-ruby-todo-list.herokuapp.com/' target="_blank">
-                                <img src={ todolist } width='64%' height='auto' />
-                                <h2 id='reveal'>Full Stack To Do List</h2>
-                                <br/>
-                            </a>
-                        </div>
-                        <div className='col-0 col-md-2'></div>
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 pt-5 mb-3' id='webdev'>
+                        <a href='https://jf-fullstack-ruby-todo-list.herokuapp.com/' target="_blank">
+                            <img src={ todolist } width='64%' height='auto' />
+                            <h2 id='reveal'>Full Stack To Do List</h2>
+                            <br/>
+                        </a>
+                    </div>
+                    <div className='col-0 col-md-2'></div>
+
+                    <div className='col-0 col-md-2'></div>
+                    <div className='col-12 col-md-8 pt-5 mb-3' id='webdev'>
+                        <a href='https://rails-fs-twitter-clone.herokuapp.com/' target="_blank">
+                            <img src={ twitter } width='64%' height='auto' />
+                            <h2 id='reveal'>React in RoR Twitter Clone</h2>
+                            <br/>
+                        </a>
+                    </div>
+                    <div className='col-0 col-md-2'></div>
                 </div>
             </div>
         </React.Fragment>
