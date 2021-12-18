8bb8f8a56313ae91786af7fa32bb03e3e6aac51a
jmferg11 <jfergie93@gmail.com>
2021-12-17 21:11:04 -0500

    add snake.js

---
+++ src/snake.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Snake = () => {
+    return (
+        <React.Fragment>
+            <h1>hallo</h1>
+        </React.Fragment>
+    )
+}
+
+export default Snake;
